84a19b23fda9c9da573ebd88afa1e2405c8bd25c
Erik van Weelderen <erikvanweelderen@hotmail.nl>
2023-06-16 17:15:17 +0200

    working

--- include/modules/control_module/main_controller_defines.hpp
+++ include/modules/control_module/main_controller_defines.hpp
@@ -11,25 +11,27 @@
 
 #ifdef USE_SCREEN
 
-#define USER_SETUP_LOADED
-#define ST7796_DRIVER
+// These values are defined in the platformio.ini, otherwise the code does not function, however, here is a list of the pin defines.
 
-#define TFT_RST 32
-#define TFT_MISO 19
-#define TFT_MOSI 23
-#define TFT_SCLK 18
-#define TFT_CS 4
-#define TFT_DC 2
+// #define USER_SETUP_LOADED
+// #define ST7796_DRIVER
 
-#define LOAD_GLCD
-#define LOAD_FONT2
-#define LOAD_FONT4
-#define LOAD_FONT6
-#define LOAD_FONT7
-#define LOAD_FONT8
-#define LOAD_GFXFF
-#define SMOOTH_FONT
-#define SPI_FREQUENCY 27000000
+// #define TFT_RST 32
+// #define TFT_MISO 19
+// #define TFT_MOSI 23
+// #define TFT_SCLK 18
+// #define TFT_CS 4
+// #define TFT_DC 2
+
+// #define LOAD_GLCD
+// #define LOAD_FONT2
+// #define LOAD_FONT4
+// #define LOAD_FONT6
+// #define LOAD_FONT7
+// #define LOAD_FONT8
+// #define LOAD_GFXFF
+// #define SMOOTH_FONT
+// #define SPI_FREQUENCY 27000000
 
 #endif
 
